89b2e0464f4f960962c066ec99c5a4dcfee5ad54
Michael Duan <69947599+duanmike@users.noreply.github.com>
2020-10-03 10:55:50 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -62,6 +62,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     subject = "Subject: Testing subject\r\n\r\n"
     clientSocket.send(subject.encode())
     clientSocket.send(msg.encode())
+    clientSocket.send(endmsg.encode())
     recv5 = clientSocket.recv(1024).decode()
     #print(recv5)
     #if recv5[:3] != '250':
